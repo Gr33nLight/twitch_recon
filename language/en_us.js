c516a2e54a5960478168503bb70f0756ee35399c
Gr33nLight <bdepaula520@gmail.com>
2021-04-29 23:55:20 +0200

    fix(API): pagination results

--- language/en_us.js
+++ language/en_us.js
@@ -3,4 +3,5 @@ export let en_us = {
   ERR_CHANNEL_NO_VODS: 'The channel has no videos',
   ERR_SRC_FORMAT: 'Invalid source format',
   ERR_UNKNOWN: 'Unknown Error',
+  ERR_VOD_NOT_FOUND: 'Unable to find VOD for the required channel',
 };
